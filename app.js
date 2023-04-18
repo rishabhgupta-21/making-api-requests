8654267d01fcd6646fe8ef5905e7aa33bff4a11e
rishabhgupta-21 <contctrishabh@gmail.com>
2023-04-19 02:49:08 +0530

    modified index.html, styles.css, and app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// https://uselessfacts.jsph.pl/api/v2/facts/today
+
+const factsList = document.querySelector('#factsList')
+const fetchCallbackButton = document.querySelector('#fetchCallback')
+const fetchAsyncButton = document.querySelector('#fetchAsync')
+const axiosCallbackButton = document.querySelector('#axiosCallback')
+const axiosAsyncButton = document.querySelector('#axiosAsync')
