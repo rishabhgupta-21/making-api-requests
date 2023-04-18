f96334cf4ff512cf5254de2c5c0090ea5064b1aa
rishabhgupta-21 <contctrishabh@gmail.com>
2023-04-19 03:54:10 +0530

    fetch useless facts button functionality added

--- app.js
+++ app.js
@@ -5,3 +5,56 @@ const fetchCallbackButton = document.querySelector('#fetchCallback')
 const fetchAsyncButton = document.querySelector('#fetchAsync')
 const axiosCallbackButton = document.querySelector('#axiosCallback')
 const axiosAsyncButton = document.querySelector('#axiosAsync')
+
+fetchCallbackButton.addEventListener('click', () => {
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        }
+    }
+
+    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random', config)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            const newFact = document.createElement('li');
+            newFact.append(data.text)
+            newFact.classList.add('fact');
+            factsList.append(newFact)
+        })
+        .catch(err => {
+            console.error(err);
+
+            const p = document.createElement('p')
+            p.innerText = 'Sorry, no new facts are available right now :('
+            p.classList.add('error')
+            factsList.append(p)
+        })
+})
+
+fetchAsyncButton.addEventListener('click', async () => {
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        }
+    }
+
+    try {
+        const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random', config)
+        const data = await response.json();
+
+        const newFact = document.createElement('li');
+        newFact.append(data.text)
+        newFact.classList.add('fact');
+        factsList.append(newFact)
+    }
+    catch (err) {
+        console.error(err);
+
+        const p = document.createElement('p')
+        p.innerText = 'Sorry, no new facts are available right now :('
+        p.classList.add('error')
+        factsList.append(p)
+    }
+})
