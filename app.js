d91502561ceb0a9a018c867b611c382af91b9241
rishabhgupta-21 <contctrishabh@gmail.com>
2023-04-19 13:01:12 +0530

    optimized axios buttons functionality

--- app.js
+++ app.js
@@ -6,66 +6,22 @@ const fetchAsyncButton = document.querySelector('#fetchAsync')
 const axiosCallbackButton = document.querySelector('#axiosCallback')
 const axiosAsyncButton = document.querySelector('#axiosAsync')
 
-fetchCallbackButton.addEventListener('click', () => {
-    const config = {
-        headers: {
-            Accept: 'application/json'
-        }
+const config = {
+    headers: {
+        Accept: 'application/json'
     }
+}
 
-    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random', config)
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            const newFact = document.createElement('li');
-            newFact.append(data.text)
-            newFact.classList.add('fact');
-            factsList.append(newFact)
-        })
-        .catch(err => {
-            console.error(err);
+function addErrorFact(err) {
+    console.error(err);
 
-            const p = document.createElement('p')
-            p.innerText = 'Sorry, no new facts are available right now :('
-            p.classList.add('error')
-            factsList.append(p)
-        })
-})
-
-fetchAsyncButton.addEventListener('click', async () => {
-    const config = {
-        headers: {
-            Accept: 'application/json'
-        }
-    }
-
-    try {
-        const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random', config)
-        const data = await response.json();
-
-        const newFact = document.createElement('li');
-        newFact.append(data.text)
-        newFact.classList.add('fact');
-        factsList.append(newFact)
-    }
-    catch (err) {
-        console.error(err);
-
-        const p = document.createElement('p')
-        p.innerText = 'Sorry, no new facts are available right now :('
-        p.classList.add('error')
-        factsList.append(p)
-    }
-})
+    const p = document.createElement('p')
+    p.innerText = 'Sorry, no new facts are available right now :('
+    p.classList.add('error')
+    factsList.append(p)
+}
 
 axiosCallbackButton.addEventListener('click', () => {
-    const config = {
-        headers: {
-            Accept: 'application/json'
-        }
-    }
-
     axios.get('https://uselessfacts.jsph.pl/api/v2/facts/random', config)
         .then(response => {
             const newFact = document.createElement('li')
@@ -74,22 +30,11 @@ axiosCallbackButton.addEventListener('click', () => {
             factsList.append(newFact)
         })
         .catch(err => {
-            console.error(err)
-
-            const p = document.createElement('p')
-            p.innerText = 'Sorry, no new facts are available right now :('
-            p.classList.add('error')
-            factsList.append(p)
+            addErrorFact(err)
         })
 })
 
 axiosAsyncButton.addEventListener('click', async () => {
-    const config = {
-        headers: {
-            Accept: 'application/json'
-        }
-    }
-
     try {
         const response = await axios.get('https://uselessfacts.jsph.pl/api/v2/facts/random', config)
 
@@ -99,11 +44,6 @@ axiosAsyncButton.addEventListener('click', async () => {
         factsList.append(newFact)
     }
     catch (err) {
-        console.error(err)
-
-        const p = document.createElement('p')
-        p.innerText = 'Sorry, no new facts are available right now :('
-        p.classList.add('error')
-        factsList.append(p)
+        addErrorFact(err)
     }
 })
